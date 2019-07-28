64f8058f0f7506bd39cec8ddd54ed6e35ba57b21
Diogo Fernandes <dfop02@hotmail.com>
2019-07-27 22:11:47 -0300

    fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_27_234624) do
+ActiveRecord::Schema.define(version: 2019_07_27_234040) do
 
   create_table "characters", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -30,14 +30,6 @@ ActiveRecord::Schema.define(version: 2019_07_27_234624) do
     t.string "item_type"
     t.integer "character_id"
     t.boolean "active", default: true, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "weapons", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.string "weapon_type"
-    t.integer "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
